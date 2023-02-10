2f9d1368dcb07d39371c0e465b9094e0aee7f351
DESKTOP-B4SGJAI\shira <shiraba28@gmail.com>
2023-02-10 15:52:02 +0200

    add sidebar SearchBox

--- our-website/frontend/src/App.js
+++ our-website/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import {
   BrowserRouter,
   BrowserRouter as Router,
@@ -10,7 +10,7 @@ import ProductScreen from './screens/ProductScreen';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Store } from './Store';
-import { Container, Navbar } from 'react-bootstrap';
+import { Button, Container, Navbar } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
 import CartScreen from './screens/CartScreen';
@@ -25,6 +25,12 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import { getError } from './utils';
+import axios from 'axios';
+import { Toast } from 'react-bootstrap';
+import SearchBox from './components/SearchBox';
+
+
 
 export default function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -37,19 +43,40 @@ export default function App() {
     localStorage.removeItem('paymentMethod');
   };
 
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories , setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        Toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div className={sidebarIsOpen 
+        ? "d-flex flex-column site-container active-cont" 
+        : "d-flex flex-column site-container"}>
         <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
+            <Button variant="dark" onClick={()=>setSidebarIsOpen (!sidebarIsOpen)}>
+            <i className="fas fa-bars"></i>
+            </Button>
               <LinkContainer to="/">
                 <Navbar.Brand>amazon</Navbar.Brand>
               </LinkContainer>
 
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+              <SearchBox/>
                 <Nav className="me-auto  w-100  justify-content-end">
                   <Link to="/cart" className="nav-link">
                     Cart
@@ -86,6 +113,29 @@ export default function App() {
             </Container>
           </Navbar>
         </header>
+        <div
+        className={
+          sidebarIsOpen
+            ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+            : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+        }
+      >
+      <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={{ pathname: '/search', search: `category=${category}` }}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+      </div>
         <main>
           <Container className="mt-3">
             <Routes>
