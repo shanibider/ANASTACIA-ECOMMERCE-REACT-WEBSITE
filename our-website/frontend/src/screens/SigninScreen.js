5a68d5958f5d3fde2caa68c10b730bdd06bf5460
DESKTOP-B4SGJAI\shira <shiraba28@gmail.com>
2023-02-12 13:21:31 +0200

    sign in screen update

--- our-website/frontend/src/screens/SigninScreen.js
+++ our-website/frontend/src/screens/SigninScreen.js
@@ -5,7 +5,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Axios from 'axios';
 import { Store } from '../Store';
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { getError } from '../utils';
 
 export default function SigninScreen() {
