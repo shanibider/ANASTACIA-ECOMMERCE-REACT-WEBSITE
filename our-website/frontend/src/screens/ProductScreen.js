9ae7ec0312dc01bbaea59d474e20a4e09c8bba13
RafaelNavon <44645278+RafaelNavon@users.noreply.github.com>
2023-02-15 21:25:31 +0200

    edited price

--- our-website/frontend/src/screens/ProductScreen.js
+++ our-website/frontend/src/screens/ProductScreen.js
@@ -163,7 +163,7 @@ function ProductScreen() {
                 numReviews={product.numReviews}
               ></Rating>
             </ListGroup.Item>
-            <ListGroup.Item>Pirce : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
             <ListGroup.Item>
               Description:
               <p>{product.description}</p>
