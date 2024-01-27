d404a670eeb9bc18990581d60e5d09588d8923b8
Shani Bider <shanibider@gmail.com>
2024-01-27 12:15:45 +0200

    update

--- our-website/backend/routes/productRoutes.js
+++ our-website/backend/routes/productRoutes.js
@@ -3,185 +3,19 @@ import Product from '../models/productModel.js'; //.js is necessary to get rid o
 import expressAsyncHandler from 'express-async-handler';
 import { isAuth, isAdmin } from '../utils.js';
 
-const productRouter = express.Router();
 
-/*
-'/api/products' 
-is the first part of the url for each "get request", added automatically by:
-app.use('/api/products', productRouter);
-in server.js
-*/
+const productRouter = express.Router(); // Creating an Express router for handling product-related routes
 
-//Define the first api for productRouter
-//return all products
+
+// Fetch all products
 productRouter.get('/', async (req, res) => {
-  const products = await Product.find();
-  res.send(products);
+  const products = await Product.find (); // Retrieve all products from the database
+  res.send (products); // Send the products as a response
 });
 
-//For Admin Product List (create new product)
-productRouter.post(
-  '/',
-  isAuth,
-  isAdmin,
-  expressAsyncHandler(async (req, res) => {
-    const newProduct = new Product({
-      name: 'sample name ' + Date.now(),
-      slug: 'sample-name-' + Date.now(),
-      image: '/images/i1.jpg',
-      price: 0,
-      category: 'sample category',
-      brand: 'sample brand',
-      countInStock: 0,
-      rating: 0,
-      numReviews: 0,
-      description: 'sample description',
-    });
-    const product = await newProduct.save();
-    res.send({ message: 'Product Created', product });
-  })
-);
 
-//Update product (by Admin)
-productRouter.put(
-  '/:id',
-  isAuth,
-  isAdmin,
-  expressAsyncHandler(async (req, res) => {
-    const productId = req.params.id;
-    const product = await Product.findById(productId);
-    if (product) {
-      product.name = req.body.name;
-      product.slug = req.body.slug;
-      product.price = req.body.price;
-      product.image = req.body.image;
-      product.category = req.body.category;
-      product.brand = req.body.brand;
-      product.countInStock = req.body.countInStock;
-      product.description = req.body.description;
-      await product.save();
-      res.send({ message: 'Product Updated' });
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  })
-);
 
-//Delete product (by Admin)
-productRouter.delete(
-  '/:id',
-  isAuth,
-  isAdmin,
-  expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      await product.remove();
-      res.send({ message: 'Product Deleted' });
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  })
-);
-
-//For creating a new review for order
-productRouter.post(
-  '/:id/reviews',
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    const productId = req.params.id;
-    const product = await Product.findById(productId);
-    if (product) {
-      if (product.reviews.find((x) => x.name === req.user.name)) {
-        return res
-          .status(400)
-          .send({ message: 'You already submitted a review' });
-      }
-
-      const review = {
-        name: req.user.name,
-        rating: Number(req.body.rating),
-        comment: req.body.comment,
-      };
-      product.reviews.push(review);
-      product.numReviews = product.reviews.length;
-      product.rating =
-        product.reviews.reduce((a, c) => c.rating + a, 0) /
-        product.reviews.length;
-      const updatedProduct = await product.save();
-      res.status(201).send({
-        message: 'Review Created',
-        review: updatedProduct.reviews[updatedProduct.reviews.length - 1],
-        numReviews: product.numReviews,
-        rating: product.rating,
-      });
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  })
-);
-
-const PAGE_SIZE = 100;
-//For Admin Product List (manage products)
-productRouter.get(
-  '/admin',
-  isAuth,
-  isAdmin,
-  expressAsyncHandler(async (req, res) => {
-    const { query } = req;
-    const page = query.page || 1;
-    const limit =
-      query.limit === 'all' ? 0 : parseInt(query.limit) || PAGE_SIZE;
-
-    const findQuery = Product.find();
-    const countQuery = Product.countDocuments();
-
-    if (limit > 0) {
-      findQuery.skip(limit * (page - 1)).limit(limit);
-    }
-    const products = await findQuery;
-    const countProducts = await countQuery;
-
-    res.send({
-      products,
-      countProducts,
-      page,
-      pages: limit > 0 ? Math.ceil(countProducts / limit) : 1,
-    });
-  })
-);
-
-//For search screen
-//api to filter products at this address (/search)
-productRouter.get(
-  '/search',
-  expressAsyncHandler(async (req, res) => {
-    const { query } = req;
-    const pageSize = query.pageSize || PAGE_SIZE;
-    const page = query.page || 1;
-    const category = query.category || '';
-    const price = query.price || '';
-    const rating = query.rating || '';
-    const order = query.order || '';
-    const searchQuery = query.query || '';
-
-    //if searchQuery is exist and not equal to all, i set this object to the quary filter
-    const queryFilter =
-      searchQuery && searchQuery !== 'all'
-        ? {
-            name: {
-              $regex: searchQuery,
-              $options: 'i', //case insensitive
-            },
-          }
-        : {};
-    //category filter
-    const categoryFilter = category && category !== 'all' ? { category } : {};
-    //rating filter
-    const ratingFilter =
-      rating && rating !== 'all'
-        ? {
-            rating: {
-              $gte: Number(rating),
+  Number(rating),
             },
           }
         : {};
@@ -208,6 +42,8 @@ productRouter.get(
         ? { createdAt: -1 }
         : { _id: -1 };
 
+
+        
     //queryFilter is the object that we pass to the find() method on the product model in mongoose
     const products = await Product.find({
       ...queryFilter,
@@ -225,6 +61,8 @@ productRouter.get(
       ...priceFilter,
       ...ratingFilter,
     });
+
+        // Send the filtered products, total count, current page, and total pages as a response
     res.send({
       products,
       countProducts,
@@ -234,33 +72,55 @@ productRouter.get(
   })
 );
 
-//For categories (for Sidebar and Search Box)
+
+// Fetch unique product categories for Sidebar and Search Box
 productRouter.get(
   '/categories',
   expressAsyncHandler(async (req, res) => {
+    // Get distinct product categories
     const categories = await Product.find().distinct('category');
-    res.send(categories);
+    res.send(categories);  // Send the categories as a response
   })
 );
 
-//api to return product info by slug (From Product Screen->useEffect)
+
+
+
+// Fetch product information by slug (From Product Screen -> useEffect)
 productRouter.get('/slug/:slug', async (req, res) => {
+
+  // Find the product by slug
   const product = await Product.findOne({ slug: req.params.slug });
   if (product) {
-    res.send(product);
+    res.send(product);  // Send the product details as a response
   } else {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
-//api to return product by id (From Product Screen->addToCartHandler)
+// Fetch product by ID (From Product Screen -> addToCartHandler)
 productRouter.get('/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById (req.params.id); // Find the product by ID
   if (product) {
-    res.send(product);
+    res.send(product); // Send the product details as a response
   } else {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
 export default productRouter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
